3a4836ffbe70b4564d4c83fb2c76a6bb1e9286cd
Everett Hanke <forgetekbusiness@gmail.com>
2024-05-07 14:05:56 -0400

    setting up basic page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Everett
+ * Pedro
+ * Nathan
+ */
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Import what we require here!
+require_once ('vendor/autoload.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Reroute to home Page @BASE
+$f3->route('GET|POST /Home', function ($f3){
+
+    //var_dump($f3->get('SESSION'));
+
+
+    //Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+
+//run Fat Free
+$f3->run();
