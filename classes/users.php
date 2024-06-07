f8a0bb2d05c1015020ba7d80be3faa0b1bc17f14
Everett Hanke <forgetekbusiness@gmail.com>
2024-06-07 00:57:56 -0400

    documentation for users

--- classes/users.php
+++ classes/users.php
@@ -4,16 +4,31 @@ class CanineUsers
 {
     private $_userName;
     private $_email;
+
+    /**
+     * Constructor for User Class
+     * @param $userName String name of User
+     * @param $email String email of user
+     */
     function __construct($userName = "N/A", $email = "N/A")
     {
         $this->_email = $email;
         $this->_userName = $userName;
     }
+
+    /**
+     * Getter for email
+     * @return String email of user
+     */
     function getEmail()
     {
         return $this->_email;
     }
 
+    /**
+     * Getter for userName
+     * @return String userName of user
+     */
     function getUserName()
     {
         return $this->_userName;
